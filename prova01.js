a36905be0b13097153156769f12d80c365a37a24
Ermesson Rodrigues <63525733+ermessonr@users.noreply.github.com>
2024-03-20 20:13:26 -0300

    Add files via upload

---
+++ prova01.js
@@ -0,0 +1,12 @@
+let peso1 = 3;
+let peso2 = 3;
+let peso3 = 4;
+
+let nota1 = 90;
+let nota2 = 85;
+let nota3 = 75;
+
+media =
+  (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
+
+console.log(media);
